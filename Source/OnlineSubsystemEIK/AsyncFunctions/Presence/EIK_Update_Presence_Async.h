360938f7ab8f368b7c5d64078beaaf7e7fc3cefb
Chippy4 <chippy.tablet@gmail.com>
2023-12-20 12:06:32 +0100

    Added Set_Presence Function

---
+++ Source/OnlineSubsystemEIK/AsyncFunctions/Presence/EIK_Update_Presence_Async.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "OnlineSubsystemEIK/Subsystem/EIK_Subsystem.h"
+
+//#include "EIK_Update_Presence_Async.generated.h"
+
+
